bf8637d45b78c2a766a02e7f8a841a39cc59a39f
dannielboy58 <akinnusotutemitayodaniel@gmail.com>
2020-04-07 16:25:34 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,11 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
+  compilers: {
+    solc: {
+      version: "0.4.50",
+    }
+  },
   networks: {
    development: {
      host: "127.0.0.1",
