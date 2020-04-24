7ae124f08aa4eb03579499abd36e5569ead428c2
Fugue Foundation <admin@fuguefoundation.org>
2020-04-24 16:26:20 -0400

    improved Block Native integration

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
 import { Web3Service } from '../services/web3.service';
+import { WalletState } from '../models/walletState';
 
 declare let require: any;
 const nft_artifacts = require('../../../build/contracts/XFFToken.json');
@@ -13,6 +14,8 @@ const nft_artifacts = require('../../../build/contracts/XFFToken.json');
 
 export class DonateComponent implements OnInit {
   NFT: any;
+  private walletState: WalletState;
+  private networkName: String;
 
   constructor(
     private location: Location,
@@ -21,20 +24,33 @@ export class DonateComponent implements OnInit {
 
   ngOnInit(): void {
     this.web3Service.artifactsToContract(nft_artifacts)
-        .then((abstract) => {
-            this.NFT = abstract;
-        });  
+      .then((abstract) => {
+        this.NFT = abstract;
+      });
+    this.web3Service.currentWalletState$.subscribe(state => {
+        this.walletState = state;
+        this.networkName = this.getNetworkName(this.walletState.network);
+        console.log(state);
+    });
   }
 
   sendTx(): void {
-    //TODO//
-    /*
-    - Start metamask tx with correct params
-    */
-    console.log('sendTx');
+    let temp = {from: this.walletState.address, to: '0x88c98f3eCD2BDc06EE10B191165Cb9924B3F7C4b', value: "1000000"}
+    this.web3Service.sendTx(temp);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  getNetworkName(id) {
+      switch (id) {
+          case 1:
+              return 'MainNet';            
+          case 5:
+              return 'Goerli';            
+          default:
+              return 'Unknown';
+      }
   }
 }
