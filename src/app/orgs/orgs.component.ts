0ffad6a23041d2fe5a04757b61ff81c1002ba626
Fugue Foundation <admin@fuguefoundation.org>
2020-04-24 16:29:52 -0400

    minor edits to functionality

--- src/app/orgs/orgs.component.ts
+++ src/app/orgs/orgs.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Org } from '../models/org';
 import { OrgsService } from '../services/orgs.service';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-orgs',
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -34,13 +34,17 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 34, name: 'Univursa Health', evaluatorId: 4, desc: 'Description' },
       { id: 35, name: 'Development Media International', evaluatorId: 4, desc: 'Description' },
       { id: 36, name: 'Centre for Study of Existential Risk', evaluatorId: 4, desc: 'Description' },
+      { id: 37, name: 'Electronic Frontier Foundation', evaluatorId: 5, desc: 'The Electronic Frontier Foundation is the leading nonprofit organization defending civil liberties in the digital world.' },
+      { id: 38, name: 'OWASP', evaluatorId: 5, desc: 'The Open Web Application Security Project (OWASP) is a nonprofit foundation that works to improve the security of software.' },
+      { id: 39, name: 'Tor Project', evaluatorId: 5, desc: 'To advance human rights and freedoms by creating and deploying free and open source anonymity and privacy technologies, supporting their unrestricted availability and use, and furthering their scientific and popular understanding.' },
+      { id: 40, name: 'Wikimedia Foundation', evaluatorId: 5, desc: 'The Wikimedia Foundation, Inc is a nonprofit charitable organization dedicated to encouraging the growth, development and distribution of free, multilingual content, and to providing the full content of these wiki-based projects to the public free of charge.' },
     ];
     const evaluators = [
         { id: 1, name: 'Give Well', desc: 'Description Give Well', color: '#ffffff', image: './assets/images/self-reliance.jpg'},
         { id: 2, name: 'Charity Navigator', desc: 'Description Evaluator 2', color: 'gray', image: './assets/images/group-cliff.png'},
         { id: 3, name: 'The Life You Can Save', desc: 'Description Evaluator 3', color: '#000000', image: './assets/images/woman-veil.png'},
-        { id: 4, name: 'COVID-19 | Effective Altruism', desc: 'Description Evaluator 4', color: '#b31121', image: './assets/images/synapses.jpg'}
-
+        { id: 4, name: 'COVID-19 | Effective Altruism', desc: 'Description Evaluator 4', color: '#b31121', image: './assets/images/synapses.jpg'},
+        { id: 5, name: 'Internet Freedoms', desc: 'Description Evaluator 5', color: '#b31121', image: './assets/images/internet-freedom.jpg'}
     ];
     return {orgs, evaluators};
   }
