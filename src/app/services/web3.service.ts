e175a8c7355796de8232cb2dd1d84ee4c06b4356
Fugue Foundation <admin@fuguefoundation.org>
2020-04-24 16:28:14 -0400

    improved Block Native integration

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs';
 declare let require: any;
 const Web3 = require('web3');
 const contract = require('@truffle/contract');
@@ -6,14 +7,16 @@ import { DebugService } from './debug.service';
 import Notify from 'bnc-notify';
 import Onboard from 'bnc-onboard'
 import { environment } from 'src/environments/environment';
+import { WalletState } from '../models/walletState';
 
 declare let window: any;
 
 @Injectable()
 export class Web3Service {
   private web3: any;
-  
-  //Block Native Options
+  public currentWalletState$ = new Subject<WalletState>();
+
+  //Block Native ONBOARD options
   initializationOptions = {
     dappId: environment.BLOCK_NATIVE_KEY,
     networkId: 5,
@@ -38,14 +41,36 @@ export class Web3Service {
     const contractAbstraction = contract(artifacts);
     contractAbstraction.setProvider(this.web3.currentProvider);
     this.log('artifactsToContract called');
-
     return contractAbstraction;
   }
 
   private async blockNativeOnboard(options) {
-    const onboard = Onboard(options)
-    await onboard.walletSelect();
-    await onboard.walletCheck();
+    const onboard = Onboard(options);
+    let walletSelected;
+    let readyToTransact;
+    try {
+        walletSelected = await onboard.walletSelect();
+    } catch (error) {
+        console.log(error);
+    }
+    if (walletSelected) {
+        readyToTransact = await onboard.walletCheck();
+    }
+    if (walletSelected && readyToTransact){
+        this.currentWalletState$.next(onboard.getState());
+    } else {
+        this.log('No wallet detected');
+    }
+  }
+
+  public sendTx(options) {
+      this.web3.eth.sendTransaction(options).on('transactionHash', function(hash){  
+        let notifyInstance = Notify({
+            dappId: environment.BLOCK_NATIVE_KEY,
+            networkId: 5  
+        });
+        notifyInstance.hash(hash);
+      })
   }
 
   /** Log a OrgService message with the MessageService */
