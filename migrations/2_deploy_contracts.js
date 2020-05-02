4fd68ad990739d7e498e44e6ad5d175747bfc5b4
Fugue Foundation <admin@fuguefoundation.org>
2020-05-02 13:19:13 -0400

    updates based on ff-contracts repo in smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,20 @@
-const XFFToken = artifacts.require("XFFToken");
+const FFKudos = artifacts.require("FFKudos");
+const FFPaymentSplit = artifacts.require("FFPaymentSplit");
 const name = "FugueFoundation";
-const symbol = "XFF";
+const symbol = "FF";
+const payees = ["address_here"]
+const evaluatorIds = [1];
 
 module.exports = function(deployer, network) {
 
     console.log(`${"-".repeat(30)}
-    DEPLOYING XFFToken Contract...\n
+    DEPLOYING FFKudos Contract...\n
     Using ` + network + ` network\n`);
 
-  deployer.deploy(XFFToken, name, symbol);
+    deployer.deploy(FFKudos, name, symbol);
 
+    console.log(`${"-".repeat(30)}
+    DEPLOYING FFPaymentSplit Contract...\n`);
+
+    deployer.deploy(FFPaymentSplit, payees, evaluatorIds);
 };
